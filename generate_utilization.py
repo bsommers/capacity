837f8108eb65569e3c2312be306869f5ce25fbb8
William r Sommers <william_r_sommers@hairy.local>
2017-01-23 14:02:05 -0500

    build composite workload

--- generate_utilization.py
+++ generate_utilization.py
@@ -12,7 +12,9 @@ from random import random
 import matplotlib.pyplot as mplt
 
 DAY_SEC=1440
-WORK_DAY=8*60
+WORK_DAY=9*60
+PRE_WORK = 8*60
+POST_WORK = 7*60
 
 def baseline():
     return 10+random()*10
@@ -25,11 +27,25 @@ def first_term(x):
 def second_term(x,magnitude):
     return magnitude * abs(sin(x*pi/WORK_DAY))
 
-cpu_util = [baseline() + first_term(x) + second_term(x,20) for x in range(DAY_SEC)]
+def backup(i):
+    if i < POST_WORK*.30 or i > POST_WORK*0.6:
+        return 5+random()*10
+    else:
+        return 30+random()*60
+
+a_cpu_util = [baseline() + backup(_) for _ in range(PRE_WORK)]
+prime_cpu_util = [baseline() + first_term(x) + second_term(x,20) 
+                    for x in range(WORK_DAY)]
 sec_term = [second_term(x,20) for x in range(DAY_SEC)]
+b_cpu_util = [baseline() for _ in range(POST_WORK)]
+
+day_workload= a_cpu_util + prime_cpu_util + b_cpu_util
 
 print(cpu_util)
-mplt.plot(cpu_util)
+#mplt.plot(cpu_util)
 mplt.axis([0, 1440, 0, 100])
-mplt.plot(sec_term)
+#mplt.plot(sec_term)
+#mplt.plot(a_cpu_util)
+#mplt.plot(b_cpu_util)
+mplt.plot(day_workload)
 mplt.show()
