ba28c6e9b59f6761735e161ca1a6808162277ee5
Bill Sommers <bill.sommers@gmail.com>
2017-01-24 15:29:51 -0500

    updates to workload model

--- generate_utilization.py
+++ generate_utilization.py
@@ -21,11 +21,11 @@ def baseline():
     
 phase_shift = pi/WORK_DAY/2
 
-def first_term(x):
-    return 50*abs(sin(x*( (2/3)* pi/WORK_DAY)+phase_shift))
+def first_term(x, magnitude):
+    return magnitude*abs(sin(x*(2* pi/WORK_DAY)+phase_shift))
 
 def second_term(x,magnitude):
-    return magnitude * abs(sin(x*pi/WORK_DAY))
+    return magnitude * abs(cos(x*2*pi/WORK_DAY))
 
 def backup(i):
     if i < POST_WORK*.30 or i > POST_WORK*0.6:
@@ -34,14 +34,14 @@ def backup(i):
         return 30+random()*60
 
 a_cpu_util = [baseline() + backup(_) for _ in range(PRE_WORK)]
-prime_cpu_util = [baseline() + first_term(x) + second_term(x,20) 
+prime_cpu_util = [baseline() + first_term(x,50) + second_term(x,20) 
                     for x in range(WORK_DAY)]
 sec_term = [second_term(x,20) for x in range(DAY_SEC)]
 b_cpu_util = [baseline() for _ in range(POST_WORK)]
 
 day_workload= a_cpu_util + prime_cpu_util + b_cpu_util
 
-print(cpu_util)
+print(day_workload)
 #mplt.plot(cpu_util)
 mplt.axis([0, 1440, 0, 100])
 #mplt.plot(sec_term)
